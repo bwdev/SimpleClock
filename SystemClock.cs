412a54886bb4a3b6d983a3ddcf9723fc8bdf13c3
Brian Meek <bmeek@Brians-MBP.attlocal.net>
2021-02-14 15:27:44 -0600

    initial commit

---
+++ SystemClock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SystemClock.Core
+{
+    public interface IClock
+    {
+        DateTime Now { get; }
+        DateTime UtcNow { get; }
+    }
+
+    public class Clock : IClock
+    {
+        public DateTime Now => DateTime.Now;
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+
+    public static class ClockExtension
+    {
+        public static void AddClock(this IServiceCollection services)
+        {
+            services.AddSingleton<IClock, Clock>();
+        }
+    }
+
+}
